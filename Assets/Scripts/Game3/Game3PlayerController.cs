a4379330ccfca51c7a4685d0b00f06cb8479d4d4
Siyn0 <3407481422@qq.com>
2021-11-24 18:21:24 +0800

    Start S11

---
+++ Assets/Scripts/Game3/Game3PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game3PlayerController : MonoBehaviour
+{
+    public float MoveSpeed = 0.1f;
+
+    private void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+
+        }
+    }
+}
