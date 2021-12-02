467a6df9c36ad55b4b0751741a71fb5dbcf1fd85
Siyn0 <3407481422@qq.com>
2021-12-02 17:43:00 +0800

    UpdateS11

---
+++ Assets/Scripts/Game3/EnemyMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float MoveSpeed = 0.1f;
+
+    private void Update()
+    {
+        transform.position += new Vector3(-MoveSpeed, 0f, 0f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.gameObject.GetComponent<Game3PlayerController>().LoseHP();
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Game3/Game3PlayerController.cs
+++ Assets/Scripts/Game3/Game3PlayerController.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game3PlayerController : MonoBehaviour
 {
     public float MoveSpeed = 0.1f;
+    private int HP = 100;
+    public Image HPBar;
+    public GameObject DiePanel;
 
     private void Update()
     {
         if(Input.GetAxis("Horizontal") > 0)
         {
+            transform.position += new Vector3(MoveSpeed, 0f, 0f);
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            transform.position += new Vector3(-MoveSpeed, 0f, 0f);
+        }
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            transform.position += new Vector3(0f, MoveSpeed, 0f);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.position += new Vector3(0f, -MoveSpeed, 0f);
+        }
+    }
 
+    private void UpdateHP()
+    {
+        HPBar.fillAmount = HP / 100;
+    }
+
+    public void LoseHP()
+    {
+        HP -= 10;
+        UpdateHP();
+        if (HP <= 0)
+        {
+            DiePanel.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 }
